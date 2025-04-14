e82956b3abf62c730967a2e66a3d2816fe8e7a6d
Zeinep Zhorobekova <zeinepzhorobekova@gmail.com>
2025-04-14 07:55:02 +0000

    Your meaningful commit message here

---
+++ PE01-HelloWorld/app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Zeinep Zhorobekova</Text>
+      <Text style={styles.text}>MS in Computer Science</Text>
+      <Text style={styles.text}>City University of Seattle</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    margin: 10,
+  },
+});
